97548ccdd5b5faaaf9e6f8c496d51515efef18cf
jaclyn.ashley.ramkissoon@live.mercer.edu <Jaclyn Ramkissoon>
2014-12-02 09:53:21 -0500

    final

---
+++ main.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+    @title = "Jaclyn Ramkissoon's Portfolio"
+    @description = "This site showcases all of the awesome things that Jaclyn Ramkissoon has done."
+    @home = "active"
+    erb :onepage
+end
